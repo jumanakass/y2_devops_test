41a8880b20773a603f3a3ff5cf702016a502ce8b
jumana <jumana.kass@microfocus.com>
2020-01-18 13:01:22 +0200

    editing entrypoint

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+IMAGE_NAME = "yad2imagenew"
+TAG ="jumana"
+#Registry --> hub.docker.com
 echo "Hello from brnach: ${GITHUB_REF##*/}"
+
+docker build -t ${IMAGE_NAME} .
+docker tag ${IMAGE_NAME}:${TAG}
+
+
+
+
+
